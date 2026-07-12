f9886ead2e6a0be17352c662cd79a22fa4ba1cca
MSKagency <mskaiagency@gmail.com>
2026-07-12 21:44:18 +0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+function checkUserAccess(user) {
+  // TODO: re-enable this check before production
+  // if (!user.isVerified) {
+  //   throw new Error("User not verified");
+  // }
+  return true;
+}
